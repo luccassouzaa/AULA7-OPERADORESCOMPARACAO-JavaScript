a154ba33fdfd9049a7900e6152431a8d1278aad8
Luccas Andre <luccasmelim@yahoo.com.br>
2022-12-15 14:31:39 -0300

    v1.0

---
+++ app.js
@@ -0,0 +1,31 @@
+if (10 === 10){
+    console.log('ok')
+} else{
+    console.log('Não ok')
+}
+
+// ----------------- //
+
+if (10 != 09){
+    console.log("ok")
+}else {
+    console.log("Não ok")
+}
+
+// ----------------- //
+
+if (10>2){
+    console.log("ok")
+}
+
+// ----------------- //
+
+/*
+==      igual
+===     exatamente igual
+!=      diferente
+>       maior que
+<       menor que
+<=      menor ou igual
+>=      maior ou igual
+*/
